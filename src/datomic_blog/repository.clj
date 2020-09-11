36058c8c6b8f60757a346669bb87094dd9e682d9
Marco Fracassi <fracassi.marco@gmail.com>
2020-09-11 10:27:58 +0200

    retrieve a blog post key by title

--- src/datomic_blog/repository.clj
+++ src/datomic_blog/repository.clj
@@ -1,2 +1,16 @@
-(ns datomic_blog.repository)
+(ns datomic_blog.repository
+  (:require [datomic.api :as d])
+)
 
+(def db-uri (str "datomic:mem://" (d/squuid)))
+
+(defn connection []
+  (d/connect db-uri)
+)
+
+(defn blog-post-key-by-title [title]
+  (let [result (d/q '[:find ?e :in $ ?title :where [?e :post/title ?title]] (d/db (connection)) title)]
+    (println "result --> " result)
+    (first (first result))
+  )
+)
--- test/datomic_blog/repository_test.clj
+++ test/datomic_blog/repository_test.clj
@@ -1,49 +1,37 @@
 (ns datomic_blog.repository_test
   (:require [clojure.test :refer :all]
-              [datomic_blog.repository :refer :all]
+            [datomic_blog.repository :refer :all]
             [datomic.api :as d]
   )
 )
 
-(def db-uri-base "datomic:mem://")
-(defn scratch-conn
-  "Create a connection to an anonymous, in-memory database."
-  []
-  (let [uri (str db-uri-base (d/squuid))]
-    (d/delete-database uri)
-    (d/create-database uri)
-    (d/connect uri)))
-
 (def post-schema [{
-                      :db/ident       :post/title
-                      :db/valueType   :db.type/string
-                      :db/cardinality :db.cardinality/one
-                      }
-                     {
-                      :db/ident       :post/body
-                      :db/valueType   :db.type/string
-                      :db/cardinality :db.cardinality/one
-                      }])
-(def conn (scratch-conn))
+                    :db/ident       :post/title
+                    :db/valueType   :db.type/string
+                    :db/cardinality :db.cardinality/one
+                    }
+                   {
+                    :db/ident       :post/body
+                    :db/valueType   :db.type/string
+                    :db/cardinality :db.cardinality/one
+                    }]
+)
 
 (defn create-db []
-  (d/transact conn post-schema)
+  (d/create-database db-uri)
+  (d/transact (connection) post-schema)
 )
 
 (defn destroy-db []
-  (println "dopo")
+  (d/delete-database db-uri)
 )
 
 (defn prepare-db [f] (create-db)  (f) (destroy-db))
 
 (use-fixtures :once prepare-db)
 
-(deftest a-test
-  (d/transact conn [{:post/title "new blogpost" :post/body "content"}])
-  (let [results (d/q '[:find ?e ?body
-                       :where [?e :post/title "new blogpost"]
-                              [?e :post/body ?body]] (d/db conn))]
+(deftest retrieve-a-blog-post-key-by-title
+  (d/transact (connection) [{:post/title "new blogpost" :post/body "content"}])
 
-    (is (= (second (first results)) "content"))
-  )
+  (is (not= (blog-post-key-by-title "new blogpost") nil))
 )
