b67bfec4e44e94bc8d6580be3cd4880b60be8af4
Marco Fracassi <fracassi.marco@gmail.com>
2020-09-16 17:00:46 +0200

    Remove default repl command

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A Clojure kata designed to lear Datomic"
   :url "https://github.com/marco-fracassi-milkman/datomic-blog-kata"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [com.datomic/datomic-free "0.9.5656"]]
-  :repl-options {:init-ns datomic-blog.core})
+  )
